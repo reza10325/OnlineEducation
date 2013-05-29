a56898c91a3333a1e7c79aa05c3b454cb94c23c4
noori <jnoori1985@gmail.com>
2013-05-29 11:32:11 +0430

    edit for Answer&Question tables

--- database/online_education.sql
+++ database/online_education.sql
@@ -23,6 +23,90 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+
+--
+-- Table structure for table `answers`
+--
+
+CREATE TABLE IF NOT EXISTS `answers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `questions_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `questions_id` (`questions_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers_given`
+--
+
+CREATE TABLE IF NOT EXISTS `answers_given` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `answers_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `answers_id` (`answers_id`,`users_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE IF NOT EXISTS `courses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fields_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fields_id` (`fields_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses_detail`
+--
+
+CREATE TABLE IF NOT EXISTS `courses_detail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `courses_id` int(11) NOT NULL,
+  `status` enum('articles','documents') COLLATE utf8_persian_ci NOT NULL DEFAULT 'articles',
+  `link` text COLLATE utf8_persian_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `courses_id` (`courses_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam`
+--
+
+CREATE TABLE IF NOT EXISTS `exam` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `courses_id` int(11) NOT NULL,
+  `creator_id` int(11) NOT NULL COMMENT 'managers id',
+  PRIMARY KEY (`id`),
+  KEY `courses_id` (`courses_id`),
+  KEY `creator_id` (`creator_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fields`
+--
+
+CREATE TABLE IF NOT EXISTS `fields` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+
 -- Table structure for table `managers`
 --
 
@@ -57,6 +141,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `melli_code` (`melli_code`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
