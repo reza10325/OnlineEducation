d6802142a20eec7e49b8c0619275d389647230f1
Mohammad Alipour <md.alipour91@gmail.com>
2013-05-29 17:05:36 +0430

    Add Tables to Project

--- database/online_education.sql
+++ database/online_education.sql
@@ -1,26 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 3.5.8.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 29, 2013 at 04:42 PM
--- Server version: 5.5.31-0ubuntu0.13.04.1
--- PHP Version: 5.4.9-4ubuntu2
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Database: `online_education`
 --
 
 -- --------------------------------------------------------
+
+--
 
 --
 -- Table structure for table `answers`
@@ -91,6 +75,10 @@ CREATE TABLE IF NOT EXISTS `exam` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `courses_id` int(11) NOT NULL,
   `creator_id` int(11) NOT NULL COMMENT 'managers id',
+  `time_exam` int(11) NOT NULL COMMENT 'modat zamane exam',
+  `date_exam` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'tarikhe exam',
+  `item_number` int(11) NOT NULL,
+  `randomize` enum('yes','no') COLLATE utf8_persian_ci NOT NULL DEFAULT 'yes',
   PRIMARY KEY (`id`),
   KEY `courses_id` (`courses_id`),
   KEY `creator_id` (`creator_id`)
@@ -110,13 +98,19 @@ CREATE TABLE IF NOT EXISTS `fields` (
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `managers`
 --
 
 CREATE TABLE IF NOT EXISTS `managers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` enum('admin','financial','teacher') COLLATE utf8_persian_ci NOT NULL DEFAULT 'admin',
-  PRIMARY KEY (`id`)
+  `name` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `family` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `username` varchar(50) COLLATE utf8_persian_ci NOT NULL,
+  `password` varchar(32) COLLATE utf8_persian_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -156,9 +150,18 @@ CREATE TABLE IF NOT EXISTS `questions` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
+  `name` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `family` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `mobile` int(11) NOT NULL,
+  `age` int(3) NOT NULL,
+  `gender` enum('male','female') COLLATE utf8_persian_ci NOT NULL,
+  `melli_code` int(10) NOT NULL,
+  `email` varchar(50) COLLATE utf8_persian_ci NOT NULL,
+  `field_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `melli_code` (`melli_code`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
