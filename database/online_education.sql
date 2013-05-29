9253977e1cc97101b5ee545016ccabd5b593eb8c
noori <jnoori1985@gmail.com>
2013-05-29 11:13:27 +0430

    change user&manager tables in orginal file

--- database/online_education.sql
+++ database/online_education.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 28, 2013 at 08:32 PM
+-- Generation Time: May 28, 2013 at 06:10 PM
 -- Server version: 5.5.31-0ubuntu0.13.04.1
 -- PHP Version: 5.4.9-4ubuntu2
 
@@ -23,128 +23,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `answers`
---
-
-CREATE TABLE IF NOT EXISTS `answers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `questions_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `questions_id` (`questions_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `answers_given`
---
-
-CREATE TABLE IF NOT EXISTS `answers_given` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `answers_id` int(11) NOT NULL,
-  `users_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `answers_id` (`answers_id`,`users_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `courses`
---
-
-CREATE TABLE IF NOT EXISTS `courses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fields_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fields_id` (`fields_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `courses_detail`
---
-
-CREATE TABLE IF NOT EXISTS `courses_detail` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `courses_id` int(11) NOT NULL,
-  `status` enum('articles','documents') COLLATE utf8_persian_ci NOT NULL DEFAULT 'articles',
-  `link` text COLLATE utf8_persian_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `courses_id` (`courses_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `exam`
---
-
-CREATE TABLE IF NOT EXISTS `exam` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `courses_id` int(11) NOT NULL,
-  `creator_id` int(11) NOT NULL COMMENT 'managers id',
-  `time_exam` int(11) NOT NULL COMMENT 'modat zamane exam',
-  `date_exam` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'tarikhe exam',
-  `item_number` int(11) NOT NULL,
-  `randomize` enum('yes','no') COLLATE utf8_persian_ci NOT NULL DEFAULT 'yes',
-  PRIMARY KEY (`id`),
-  KEY `courses_id` (`courses_id`),
-  KEY `creator_id` (`creator_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `fields`
---
-
-CREATE TABLE IF NOT EXISTS `fields` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `managers`
 --
 
 CREATE TABLE IF NOT EXISTS `managers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` enum('admin','financial','teacher') COLLATE utf8_persian_ci NOT NULL DEFAULT 'admin',
-  PRIMARY KEY (`id`)
+  `name` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `family` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `username` varchar(50) COLLATE utf8_persian_ci NOT NULL,
+  `password` varchar(32) COLLATE utf8_persian_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `message`
---
-
-CREATE TABLE IF NOT EXISTS `message` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sender_id` int(11) NOT NULL COMMENT 'id from users or managers table (check sender type)',
-  `reciver_id` int(11) NOT NULL COMMENT 'id from users or managers table (check reciver type)',
-  `sender_type` enum('managers','users') COLLATE utf8_persian_ci NOT NULL,
-  `reciver_type` enum('managers','users') COLLATE utf8_persian_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `sender_id` (`sender_id`,`reciver_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `questions`
---
-
-CREATE TABLE IF NOT EXISTS `questions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `exam_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `exam_id` (`exam_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -154,7 +45,16 @@ CREATE TABLE IF NOT EXISTS `questions` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
+  `name` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `family` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `mobile` int(11) NOT NULL,
+  `age` int(3) NOT NULL,
+  `gender` enum('male','female') COLLATE utf8_persian_ci NOT NULL,
+  `melli_code` int(10) NOT NULL,
+  `email` varchar(50) COLLATE utf8_persian_ci NOT NULL,
+  `field_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `melli_code` (`melli_code`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
