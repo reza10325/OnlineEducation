ea687b082f5ba29f4ec528fefdea5aad76fd3f43
koushki <reza10325@yahoo.com>
2013-05-30 11:25:39 +0430

    Revert "edit table Question"
    This reverts commit baaa956243d5a5cffe89d984f65a24ec81464189.

--- database/online_education.sql
+++ database/online_education.sql
@@ -1,26 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 3.5.8.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 30, 2013 at 01:40 AM
--- Server version: 5.5.31-0ubuntu0.13.04.1
--- PHP Version: 5.4.9-4ubuntu2
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Database: `online_education`
 --
 
 -- --------------------------------------------------------
+
+--
 
 --
 -- Table structure for table `answers`
@@ -29,6 +13,10 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `answers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `questions_id` int(11) NOT NULL,
+  `answer_text` text COLLATE utf8_persian_ci NOT NULL,
+  `content` text COLLATE utf8_persian_ci NOT NULL,
+  `istrue` enum('yes','no') COLLATE utf8_persian_ci NOT NULL,
+  `rondomize` enum('yes','no') COLLATE utf8_persian_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `questions_id` (`questions_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
@@ -41,10 +29,12 @@ CREATE TABLE IF NOT EXISTS `answers` (
 
 CREATE TABLE IF NOT EXISTS `answers_given` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `answers_id` int(11) NOT NULL,
   `users_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `answers_id` text COLLATE utf8_persian_ci NOT NULL,
+  `success` enum('yes','no') COLLATE utf8_persian_ci NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `answers_id` (`answers_id`,`users_id`)
+  KEY `answers_id` (`users_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -108,13 +98,19 @@ CREATE TABLE IF NOT EXISTS `fields` (
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `managers`
 --
 
 CREATE TABLE IF NOT EXISTS `managers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` enum('admin','financial','teacher') COLLATE utf8_persian_ci NOT NULL DEFAULT 'admin',
-  PRIMARY KEY (`id`)
+  `name` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `family` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `username` varchar(50) COLLATE utf8_persian_ci NOT NULL,
+  `password` varchar(32) COLLATE utf8_persian_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -142,9 +138,6 @@ CREATE TABLE IF NOT EXISTS `message` (
 CREATE TABLE IF NOT EXISTS `questions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `exam_id` int(11) NOT NULL,
-  `content` text COLLATE utf8_persian_ci NOT NULL,
-  `number_of_answer` int(11) NOT NULL,
-  `randomize` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `exam_id` (`exam_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
@@ -157,9 +150,18 @@ CREATE TABLE IF NOT EXISTS `questions` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
+  `name` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `family` varchar(200) COLLATE utf8_persian_ci NOT NULL,
+  `mobile` int(11) NOT NULL,
+  `age` int(3) NOT NULL,
+  `gender` enum('male','female') COLLATE utf8_persian_ci NOT NULL,
+  `melli_code` int(10) NOT NULL,
+  `email` varchar(50) COLLATE utf8_persian_ci NOT NULL,
+  `field_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `melli_code` (`melli_code`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
