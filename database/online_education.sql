f44837d9dd9501cbc4c583dae258c7b38b7646ed
noori <jnoori1985@gmail.com>
2013-06-01 12:53:30 +0430

    add massage table

--- database/online_education.sql
+++ database/online_education.sql
@@ -1,10 +1,26 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.8.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 01, 2013 at 12:51 PM
+-- Server version: 5.5.31-0ubuntu0.13.04.1
+-- PHP Version: 5.4.9-4ubuntu2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
 -- Database: `online_education`
 --
 
 -- --------------------------------------------------------
-
---
 
 --
 -- Table structure for table `answers`
@@ -50,9 +66,9 @@ CREATE TABLE IF NOT EXISTS `courses` (
   `cepacity` int(11) NOT NULL,
   `period-time` int(11) NOT NULL,
   `start-period` date NOT NULL,
-  `end-period` date NOT NULL, 
+  `end-period` date NOT NULL,
   `document` varchar(255) COLLATE utf8_persian_ci NOT NULL,
-  `const-fixed` int(11) NOT NULL,	
+  `const-fixed` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fields_id` (`fields_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
@@ -105,7 +121,6 @@ CREATE TABLE IF NOT EXISTS `fields` (
 -- --------------------------------------------------------
 
 --
-
 -- Table structure for table `managers`
 --
 
@@ -132,6 +147,10 @@ CREATE TABLE IF NOT EXISTS `message` (
   `reciver_id` int(11) NOT NULL COMMENT 'id from users or managers table (check reciver type)',
   `sender_type` enum('managers','users') COLLATE utf8_persian_ci NOT NULL,
   `reciver_type` enum('managers','users') COLLATE utf8_persian_ci NOT NULL,
+  `title` varchar(50) COLLATE utf8_persian_ci NOT NULL,
+  `comment` longtext COLLATE utf8_persian_ci NOT NULL,
+  `date` int(10) NOT NULL,
+  `attache` text COLLATE utf8_persian_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `sender_id` (`sender_id`,`reciver_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
@@ -172,6 +191,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `melli_code` (`melli_code`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci AUTO_INCREMENT=1 ;
 
-
-
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
