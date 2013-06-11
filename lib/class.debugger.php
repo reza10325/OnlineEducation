6abcd41315c6ecfd772da144a963aab29f341413
Koushki <koushky@gmail.com>
2013-06-11 18:38:24 +0430

    FIX SYNTAX BUG

--- lib/class.debugger.php
+++ lib/class.debugger.php
@@ -1,6 +1,6 @@
 <?php
-static class debugger{
-	function trigger() {
+class debugger{
+	static function trigger() {
 		$trace = debug_backtrace();
 		$errlog = fopen("../tmp/error.log", "a+");
 		fwrite ( $errlog , $trace );
