10a9a452dfee4becefbd033f165437b8985069c7
hparsi <parsi.moghaddam@gmail.com>
2013-06-18 00:49:06 +0430

    Add the message to debugger class

--- lib/class.debugger.php
+++ lib/class.debugger.php
@@ -1,8 +1,12 @@
 <?php
 class debugger{
 	static $ENV;
+	static $MSG;
 	static function setEnvironment($env){
 		self::$ENV = $env;
+	}
+	static function setMessage($msg){
+		self::$MSG = $msg;
 	}
 	static function trigger() {
 		switch (self::$ENV) {
@@ -12,9 +16,11 @@ class debugger{
 			    $trace = ob_get_contents();
 			    ob_end_clean();
 				$errlog = fopen("../tmp/error.log", "a+");
+				fwrite ( $errlog , self::$MSG );
 				fwrite ( $errlog , $trace );
 				break;
 			case 'developer':
+				print (self::$MSG);
 				debug_print_backtrace();
 				break;
 		}
