f0aaacda4bc532deb52fef3dbb6def06b7ea261e
hparsi <parsi.moghaddam@gmail.com>
2013-06-19 17:32:20 +0430

    Add debugger class completely

--- lib/class.debugger.php
+++ lib/class.debugger.php
@@ -2,24 +2,35 @@
 class debugger{
 	static $ENV;
 	static $MSG;
-	static function setEnvMsg($env){
+	static $START=0;
+	static $END=10;
+	static function setElement($env,$msg,$start,$end){
 		self::$ENV = $env;
 		self::$MSG = $msg;
+		self::$START= $start;
+		self::$END = $end;
 	}
-		static function trigger() {
+	static function backtrace(){
+		ob_start(); 
+        debug_print_backtrace();
+		$trace = self::$MSG; 
+        $trace = ob_get_contents(); 
+        ob_end_clean();
+		}
+	static function trigger() {
 		switch (self::$ENV) {
 			case 'production':
-				ob_start();
-		        debug_print_backtrace();
-			    $trace = ob_get_contents();
-			    ob_end_clean();
-				$errlog = fopen("../tmp/error.log", "a+");
-				fwrite ( $errlog , self::$MSG );
-				fwrite ( $errlog , $trace );
+				for($i = self::$start ; $i = self::$END ; $i++){
+					$logTarce = $trace($i);
+					$errlog = fopen("../tmp/error.log", "a+");
+					fwrite ( $errlog , $logTrace );
+				}
 				break;
 			case 'developer':
-				print (self::$MSG);
-				debug_print_backtrace();
+				for($i = self::$start ; $i = self::$END ; $i++){
+					var_dump($trace($i));
+				}
+				exit();
 				break;
 		}
 
