35cfe2e4c9501ea3f07ae7d5236751e3cfb19838
Koushki <koushky@gmail.com>
2013-06-19 20:32:41 +0430

    Edit debugger class & fix bug

--- lib/class.debugger.php
+++ lib/class.debugger.php
@@ -1,6 +1,52 @@
 <?php
 class debugger{
-	static $ENV;
+    static private $ENV = 'developer';
+    
+    static public function setEnvironment($env){
+        self::$ENV = $env;
+    }
+    
+    static public function getEnvironment(){
+        return self::$ENV;
+    }
+    
+    /*
+     * Use Like => debugger::trigger('FILE NOT FOUND');
+     */
+    static public function trigger($message = '', $exit = true, $limit = 0) {
+        $backtrace = self::getBackTrace($limit);
+        array_unshift($backtrace, $message . "\n");
+        array_push($backtrace, str_repeat('=',50)."\n");
+        switch (self::getEnvironment()) {
+			case 'production':
+					$log = fopen(ERROR_FILE, "a+");
+                    fwrite ($log, $backtrace);
+                    fclose($log);
+                    $exit && exit();
+				break;
+            case 'developer':
+                    print implode("<br />",$backtrace);
+                    exit();
+                break;
+        }
+        return true;
+    }
+
+    static private function getBackTrace($limit = 0){
+        $trace = array();
+        $limit += 2;
+        foreach (debug_backtrace() as $k => $v) { 
+            if ($k < $limit) { 
+                continue; 
+            }
+            $trace []= '#' . ($k - $limit) . 
+                        ' [' . $v['file'] . '] at line ('. $v['line'] . ') : ' . 
+                        (isset($v['class']) ? $v['class'] . '->' : '') . $v['function'] . 
+                        '(' . @implode(', ', $v['args']) . ')' . "\n"; 
+        } 
+        return $trace;
+    }
+    /*static $ENV;
 	static $MSG;
 	static $START=0;
 	static $END=10;
@@ -34,6 +80,6 @@ class debugger{
 				break;
 		}
 
-	}
+    }*/
 }
 ?>
