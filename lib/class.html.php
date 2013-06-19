c5de032c6feb3e056df76832f2210dc2b10254bb
noori <jnoori1985@gmail.com>
2013-06-19 13:50:07 +0430

    add generateLink method

--- lib/class.html.php
+++ lib/class.html.php
@@ -139,4 +139,25 @@ class html {
 		}
 		
 	}
+	static function generateLink($method='' ,$controller=''){
+		$page=self::mainRoot(). '/' . 'index.php' ;
+		$current_p=explode('/', self::getUrl('',''));
+		$k=sizeof($current_p);
+		$current_cont=$current_p[$k-2];
+		$current_method=$current_p[$k-1];
+		$current_method_adr= self::getUrl('','');
+		array_pop($current_p);
+		$current_cont_adr=implode($current_p ,'/' );
+		array_pop($current_p);
+		$m=implode($current_p ,'/' );
+		if($method==''&& $controller==''){
+			return $main_page;
+		}
+		else if($method != '' && $controller==''){
+			return $current_cont_adr. '/' . $method;
+		}
+		else{
+			return $m. '/' . $controller . '/'. $method;
+		}
+	}
 }
