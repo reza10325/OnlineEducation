cdd0f94f254d48eeed77e08b7b371fd4768561e3
noori <jnoori1985@gmail.com>
2013-06-20 10:36:28 +0430

    edit class upload

--- lib/class.upload.php
+++ lib/class.upload.php
@@ -9,25 +9,28 @@
 		function save(){
 			if(isset($_POST[$this->submit_name])){
 				if($this->name== 'orgin'){
-					$name=$_FILES[$file_name]['name'];
+					$this->name=$_FILES[$this->file_name]['name'];
 				}
-				else if($this->name =='random'){
-					$name=$_FILES[$file_name]['tmp_name'];
+				else if($this->name =='random' || $this->name==''){
+					$temp=explode('.' , $_FILES[$this->file_name]['name']);
+					$type=$temp[sizeof($temp) -1];
+					$rnd_name=substr(md5(rand(0,999)) , 15 , 6);
+					$this->name=$rnd_name . '.' . $type;
 				}
-				$k=$this->file_name;
-				if($_FILES[$k]['error']==0 && is_uploaded_file( $_FILES[$k]['tmp_name'])
-						&& in_array($_FILES[$k]['type'], $this->allowed)){
+				if($_FILES[$this->file_name]['error']==0 
+						&& is_uploaded_file( $_FILES[$this->file_name]['tmp_name'])
+						&& in_array($_FILES[$this->file_name]['type'], $this->allowed)){
 					if($this->folder != ''){
 						$path=VIEW . DS . 'upload' . DS . $this->folder; 
 						if(!is_dir($path)){
 							mkdir($path , 777);
 						}						
-						$path=VIEW . DS . 'upload' . DS . $this->folder. DS .$_FILES[$k]['name'];
+						$path=VIEW . DS . 'upload' . DS . $this->folder. DS .$this->name;
 					}
 					else{
-						$path=VIEW . DS . 'upload' . DS .$_FILES[$k]['name'];
+						$path=VIEW . DS . 'upload' . DS .$this->name;
 					}
-					move_uploaded_file($_FILES[$k]['tmp_name'], $path);
+					move_uploaded_file($_FILES[$this->file_name]['tmp_name'], $path);
 					return $path;
 				}
 				
