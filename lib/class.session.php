6f408a9ccbbcac25177f0ef5aa05a63b9a4153a6
ali-zahedi <alizahedin1389@gmail.com>
2013-06-20 22:43:44 +0430

    class session

--- lib/class.session.php
+++ lib/class.session.php
@@ -1,28 +1,20 @@
 <?php 
-class sessions {
+class session{
 
-    function __construct()
-    { 
-        session_start(); 
+    function set( $name, $val ) { 
+        session_start();
+        $_SESSION[$var_name] = $var_val;
+	session_write_close(); 
     }
-    function set_var( $var_name, $var_val ) 
-    { 
-        if( !$var_name || !$var_val ) 
-        { 
-            return false; 
-        } 
-        $_SESSION[$var_name] = $var_val; 
+    function get( $name ){
+    	session_start(); 
+        return $_SESSION[$name]; 
+	session_write_close();
     }
-    function get_var( $var_name ) 
-    { 
-        return $_SESSION[$var_name]; 
+    function delete( $name ){
+    	session_start(); 
+        unset( $_SESSION[$name] );
+	session_write_close(); 
     }
-    function del_var( $var_name ) 
-    { 
-        unset( $_SESSION[$var_name] ); 
-    }
- function __destruct() {
-      session_destroy();
-   }
 }
 ?>
