55d7001821ebe04cb0d57dbe2362dbe7c423aff9
ali-zahedi <alizahedin1389@gmail.com>
2013-06-25 23:00:38 +0430

    edit class session

--- lib/class.session.php
+++ lib/class.session.php
@@ -1,20 +1,37 @@
-<?php 
-class session{
-
-    function set( $name, $val ) { 
-        session_start();
-        $_SESSION[$var_name] = $var_val;
-	session_write_close(); 
-    }
-    function get( $name ){
-    	session_start(); 
-        return $_SESSION[$name]; 
-	session_write_close();
-    }
-    function delete( $name ){
-    	session_start(); 
-        unset( $_SESSION[$name] );
-	session_write_close(); 
-    }
-}
-?>
+/*
+ * session::set('a','a')
+ */
+<?php
+	class session{
+		function __construct() {
+			session_start();
+		}
+		function singleton() {
+			static $instance;
+			if(empty($instance)) {
+				$instance = new self();
+			}			
+			return $instance;
+		}
+		private function set( $name , $val = null , $ttl = 1400 ){
+			$_SESSION($name) . array('expire' => time()+$ttl , 'value' => $val);
+		}
+		private function get( $name ){
+			if (!empty($_SESSION[$name]) && $_SESSION[$name]['expire'] > time()){
+				return $_SESSION[$name]['value'];
+			}
+			$this->delete($name);
+			return FALSE;
+		}
+		private function delete( $name ){
+			UNSET($_SESSION[$name]); 
+		}
+		static function __callstatic($func,$arg){
+			$object = self::singlton();
+			call_user_func_array(array($object,$func), $arg);
+		}
+		function __destruct(){
+			session_destroy(); 
+		}
+	}
+?>	
