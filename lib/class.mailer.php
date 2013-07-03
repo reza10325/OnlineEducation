3e2c7b31d8aa5c18891147a4d896ed94be59d6bf
hparsi <parsi.moghaddam@gmail.com>
2013-07-04 03:32:54 +0430

    New mailer class added with smtp class

--- lib/class.mailer.php
+++ lib/class.mailer.php
@@ -1,47 +1,26 @@
 <?php
-class SMTPClient{
-	function SMTPClient ($to, $subject, $body){
-		$this->SmtpServer = SmtpServer;
-		$this->SmtpPort = SmtpPort;
-		$this->SmtpUser = base64_encode (SmtpUser);
-		$this->SmtpPass = base64_encode (SmtpPass);
-		$this->from = From;
-		$this->to = $to;
-		$this->subject = $subject;
-		$this->body = $body;
 
-		if ($SmtpPort == ""){
-			$this->PortSMTP = 25;
-		}
-		else{
-			$this->PortSMTP = $SmtpPort;
-		}
-	}
+require_once "class.smtp.php";
 
-	function SendMail (){
-		if ($SMTPIN = fsockopen ($this->SmtpServer, $this->PortSMTP)){
-			fputs ($SMTPIN, "EHLO ".$HTTP_HOST."\r\n");
-			$talk["hello"] = fgets ( $SMTPIN, 1024 );
-			fputs($SMTPIN, "auth login\r\n");
-			$talk["res"]=fgets($SMTPIN,1024);
-			fputs($SMTPIN, $this->SmtpUser."\r\n");
-			$talk["user"]=fgets($SMTPIN,1024);
-			fputs($SMTPIN, $this->SmtpPass."\r\n");
-			$talk["pass"]=fgets($SMTPIN,256);
-			fputs ($SMTPIN, "MAIL FROM: <".$this->from.">\r\n");
-			$talk["From"] = fgets ( $SMTPIN, 1024 );
-			fputs ($SMTPIN, "RCPT TO: <".$this->to.">\r\n");
-			$talk["To"] = fgets ($SMTPIN, 1024);
-			fputs($SMTPIN, "DATA\r\n");
-			$talk["data"]=fgets( $SMTPIN,1024 );
-			fputs($SMTPIN, "To: <".$this->to.">\r\nFrom: <".$this->from.">\r\nSubject:".$this->subject."\r\n\r\n\r\n".$this->body."\r\n.\r\n");
-			$talk["send"]=fgets($SMTPIN,256);
-			//CLOSE CONNECTION AND EXIT ...
-			fputs ($SMTPIN, "QUIT\r\n");
-			fclose($SMTPIN);
-			//
-			}
-		return $talk;
-	}
+function SendMail($to,$Cc,$subject,$message){
+	$mail = new EMail;
+	
+	$mail->Username = SmtpUser;
+	$mail->Password = SmtpPass;
+	$mail->SetFrom = From;  
+	$mail->AddTo($to); 
+	$mail->AddTo($to);
+	$mail->Subject = $subject;
+	$mail->Message = $message;
+	//Optional stuff
+	$mail->AddCc($Cc);  // Set a CC if needed
+	$mail->ContentType = "text/html";          // Defaults to "text/plain; charset=iso-8859-1"
+	$mail->Headers['X-SomeHeader'] = 'abcde';  // Set some extra headers if required
+	$mail->ConnectTimeout = 30;  // Socket connect timeout (sec)
+	$mail->ResponseTimeout = 8;  // CMD response timeout (sec)
+	
+	$success = $mail->Send();
+	
 }
+
 ?>
