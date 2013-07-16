6c506f7a1f33d449047828f6d232fff9fc8c73b9
masoud-vatandoost <masoudvatandoost@ymail.com>
2013-07-16 10:37:53 +0430

    edit class.msg

--- lib/class.msg.php
+++ lib/class.msg.php
@@ -1,11 +1,12 @@
 <?php
 class msg {
-	static function setMsg($val){
+	static function set($val){
+		session_start();
 		$_SESSION['msg'] = $val;
+		session_write_close();
 	}
 	
-	static function getMsg(){
-		
+	static function get(){
 		$msg = isset($_SESSION['msg']) ? $_SESSION['msg'] : null;
 		unset($_SESSION['msg']);
 		return $msg;
