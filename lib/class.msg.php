2320d857f657001e2e6253f8ef02984de1baed3d
Mohammad Alipour <md.alipour91@gmail.com>
2013-08-04 18:00:12 +0430

    edit msg class

--- lib/class.msg.php
+++ lib/class.msg.php
@@ -1,15 +1,27 @@
 <?php
 class msg {
-	static function set($val){
-		session_start();
-		$_SESSION['msg'] = $val;
-		session_write_close();
+	static public $val;
+	static public $name;
+	static function set($name = 'defualt',$val){
+		session::set($name,$val);
 	}
 	
-	static function get(){
-		$msg = isset($_SESSION['msg']) ? $_SESSION['msg'] : null;
-		unset($_SESSION['msg']);
+	static function get($name){
+		$msg=session::get($name);
+		$msg = isset($msg) ? $msg : null;
+		session::delete($name);
 		return $msg;
 	}
+	
 }
+   		/** 
+		 * test::message class
+		 *
+		 * 	msg::set('msg','hi');
+			msg::set('msg2','bye');
+			if($msg = msg::get('msg2'))
+		    print $msg; ==> print bye 
+		 * 
+		 */
+		
 ?>
